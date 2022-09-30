b568a17a80feaa6854957fcbe281709441674ca7
mustafaaluc <mustafa.aluc@yolcu360.com>
2022-09-30 17:28:28 +0300

    some fix hook class

--- src/test/java/com/browserstack/stepdefs/Hooks.java
+++ src/test/java/com/browserstack/stepdefs/Hooks.java
@@ -1,9 +1,11 @@
 package com.browserstack.stepdefs;
 
 import com.browserstack.RunWebDriverCucumberTests;
+import com.browserstack.pageobjects.HomePage;
 import com.browserstack.pageobjects.LoginPage;
 import com.browserstack.util.BrowserUtils;
 import com.browserstack.util.ConfigurationReader;
+import com.browserstack.util.Utility;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
@@ -16,7 +18,7 @@ import java.util.concurrent.TimeUnit;
 
 public class Hooks {
     public WebDriver webDriver;
-    @Before
+  /*  @Before
     public void setUp() {
 
         webDriver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
@@ -45,9 +47,9 @@ try {
 
 }
         BrowserUtils.scrollToSize(0,-500);
-    }
+    }*/
 
-    @After
+   /* @After
     public void tearDown(Scenario scenario){
 
         if(scenario.isFailed()){
@@ -55,6 +57,39 @@ try {
             scenario.attach(screenshot,"image/png","screenshot");
         }
         webDriver.quit(); // burayı ayrıca kontrol et
+    }*/
+   @Before
+   public void setUp() {
+       webDriver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
+       webDriver = RunWebDriverCucumberTests.getManagedWebDriver().getWebDriver();
+       LoginPage loginPage = new LoginPage();
+       webDriver.manage().window().maximize();
+       webDriver.get(ConfigurationReader.get("url"));
+
+       BrowserUtils.waitForPageToLoad(30);
+
+       try {
+           if (loginPage.cookieXButton.isDisplayed()) {
+               BrowserUtils.waitForClickablility(loginPage.cookieXButton, 10);
+               loginPage.cookieXButton.click();
+           }
+       }catch (Exception e){
+
+       }
+
+   }
+
+
+
+    @After
+    public void teardown(Scenario scenario) throws Exception {
+        if (scenario.isFailed()) {
+            Utility.setSessionStatus(webDriver, "failed", String.format("%s failed.", scenario.getName()));
+        } else {
+            Utility.setSessionStatus(webDriver, "passed", String.format("%s passed.", scenario.getName()));
+        }
+        Thread.sleep(2000);
+        webDriver.quit();
     }
 
     @Before("@abc")
